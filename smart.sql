5303767312c8c3d1094337aa09a85fabf5bf50b0
timothy <mugadzatt001@gmail.com>
2018-05-21 13:39:43 +0200

    smart.sql

--- smart.sql
+++ smart.sql
@@ -68,7 +68,7 @@ WHERE
 CREATE VIEW profile_list AS 
 SELECT 
 	usp.`type`, usp.`uid`, usp.`id` AS profileid, ur.`id as relate_id,  
-	usp.`institution` AS  SCHOOL, ur.`relationship`
+	usp.`institution` AS  school , ur.`relationship`
 FROM 
 	user_profile usp, user_relationship ur 
 WHERE
