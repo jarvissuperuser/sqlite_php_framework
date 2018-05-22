0b6d60151aca6d8942138ec470bafa9d15d0b018
timothy <mugadzatt001@gmail.com>
2018-05-22 14:45:37 +0200

    model.php

--- models/model.php
+++ models/model.php
@@ -70,10 +70,11 @@ class BaseModel {
 		return [$st->fetchObject()->c,$st];
 	}
 	
-	protected function mute(&$arr){
+	protected function mute($arr){
 		foreach ($arr as $a)
 			foreach($this->muted as $d)
 				$a[$d] = "";
+		return $arr;
 	}
 	
 	protected function silence($arr){
