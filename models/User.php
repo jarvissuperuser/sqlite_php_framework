6decb7a6acea97c55bfd396aeadbfbb6cba28fa7
timothy <mugadzatt001@gmail.com>
2018-05-22 15:51:19 +0200

    User.php

--- models/User.php
+++ models/User.php
@@ -61,7 +61,7 @@ class User extends BaseModel
 	public function update($table_pointer)
 	{
 		// TODO: TestCases
-		$cols = $this->silence($this->cols[$table_pointer]);
+		$cols = $this->silence([],$this->cols[$table_pointer]);
 		$setCol = Controller::setCols($cols);
 		$values = Controller::valuesToString($setCol);
 		$id = filter_input(INPUT_POST,"userid",FILTER_SANITIZE_NUMBER_INT);
