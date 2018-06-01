d264aee56ac0c4f2c6c79d13c1096e95f4032847
timothy <mugadzatt001@gmail.com>
2018-06-01 14:46:05 +0200

    index.php

--- index.php
+++ index.php
@@ -13,25 +13,37 @@ try
 	$db = new QueryBuild();
 	$u = new User($db);
 	$p = new Profile($db);
-	
 	Controller::process_users($u);
 	Controller::process_profiles($p);
 }
 catch (PDOException $pe){
 	switch(trim(filter_input(INPUT_POST,"debug"))){
 		case '1':
-			echo json_encode(["Error"=>$pe->getTraceAsString(),$pe->getTrace()]);
-			break;
+			$auth = BaseFilter::auth("SU");
+			echo (!$auth)?
+				json_encode(
+					["Error"=>
+						$exc->getTraceAsString(),$exc->getTrace()]):
+				json_encode(
+					["Error"=>"Request Failed","success"=>false]);
 		default:
-			echo json_encode(["Error"=>"Request Failed","success"=>false]);
+			echo json_encode(["Error"=>
+				"Request Failed","success"=>false]);
 	}
 } 
 catch (Exception $exc){
 	switch(trim(filter_input(INPUT_POST,"debug"))){
 		case '1':
-			echo json_encode(["Error"=>$exc->getTraceAsString(),$exc->getTrace()]);
+			$auth = BaseFilter::auth("SU");
+			echo (!$auth)?
+				json_encode(
+					["Error"=>
+						$exc->getTraceAsString(),$exc->getTrace()]):
+				json_encode(
+					["Error"=>$exc->getMessage(),"success"=>false]);
 			break;
 		default:
-			echo json_encode(["Error"=>$exc->getMessage(),"success"=>false]);
+			echo json_encode(["Error"=>
+				$exc->getMessage(),"success"=>false]);
 	}
 }
